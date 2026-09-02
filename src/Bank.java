a9ea49b6813a34a36f0dc663c6a8575754bc118f
박장현 <janghyn97@gmail.com>
2026-09-02 16:24:02 +0900

    미션2-5 생성자와 오버로딩

--- src/Bank.java
+++ src/Bank.java
@@ -1,5 +1,5 @@
 public class Bank {
     public static void main(String[] args) {
-        System.out.println("========== AAA-BBB은행 ==========");
+        Account a = new Account("홍길동", 50000);
     }
 }
