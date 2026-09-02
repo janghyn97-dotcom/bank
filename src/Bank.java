ced0e185f47d0369c2bb24da1a9610c0cf41ac9a
박장현 <janghyn97@gmail.com>
2026-09-02 16:02:03 +0900

    conflict-demo B

--- src/Bank.java
+++ src/Bank.java
@@ -1,5 +1,5 @@
 public class Bank {
     public static void main(String[] args) {
-        System.out.println("========== OO은행 ==========");
+        System.out.println("========== BBB은행 ==========");
     }
 }
